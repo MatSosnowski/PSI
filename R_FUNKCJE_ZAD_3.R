b4809727bb6b86f7c3a1a055bbedeb11e6cd3ade
Mateusz Sosnowski <mateusz.sosnowski@student.uw.edu.pl>
2025-02-23 13:49:24 +0100

    Add files via upload

---
+++ R_FUNKCJE_ZAD_3.R
@@ -0,0 +1,8 @@
+# 3. Stwórz funkcję o nazwie pole_kola, która oblicza pole powierzchni koła dla danego promienia.
+
+pole_kola = function(r) {
+  wynik = pi * r^2
+  return(wynik)
+}
+
+pole_kola(2)
