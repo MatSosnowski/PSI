ee3c8f893dfe1abc7606978d7a735df4d715615a
Mateusz Sosnowski <mateusz.sosnowski@student.uw.edu.pl>
2025-02-23 13:54:06 +0100

    Add files via upload

---
+++ R_FUNKCJE_ZAD_4.R
@@ -0,0 +1,8 @@
+# 4. Stwórz funkcję, która oblicza długość przeciwprostokątnej w trójkącie prostokątnym.
+
+Przeciwprostokatna = function(a, b) {
+  c = sqrt(a^2 + b^2)
+  return(c)
+}
+
+Przeciwprostokatna(3,4)
